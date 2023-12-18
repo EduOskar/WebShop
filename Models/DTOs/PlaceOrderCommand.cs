15d2aa9038e7dc5bdad03d9c0d7b8373bd906c1e
oskar åhling <oskar.ahling@edu.edugrade.se>
2023-12-18 17:00:35 +0100

    Now inlogged user can browse, add to cart and do different things. A non inlogged user have to logg in to add to cart and proceed to checkout

---
+++ Models/DTOs/PlaceOrderCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebShop.Models.DTOs;
+public record PlaceOrderCommand
+{
+    public Adress BillingAdress { get; set; } = new();
+
+    public class Adress
+    {
+        public string Name { get; set; } = default!;
+
+        public string AdressLine { get; set; } = default!;
+
+        public string City { get; set; } = default!;
+
+        public string PostCode { get; set; } = default!;
+    }
+}
--- WebShop.Web/HardCoded.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-
-namespace WebShop.Web;
-
-public class HardCoded
-{
-    public const int UserId = 1;
-    public const int CartId = 1;
-}
